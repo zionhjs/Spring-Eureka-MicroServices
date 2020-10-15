e1e92aef66d3da0622133bce24dbb170526ff40f
ZionHJS <hjszion@gmail.com>
2020-10-15 10:37:01 -0700

    update

---
+++ movie-info-service/src/main/java/io/javabrains/movieinfoservice/resources/MovieResource.java
@@ -0,0 +1,26 @@
+package io.javabrains.movieinfoservice.resources;
+
+import io.javabrains.movieinfoservice.models.Movie;
+import io.javabrains.movieinfoservice.models.MovieSummary;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieResource {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @RequestMapping("/{movieId}")
+    public Movie getMovieInfo(@PathVariable("movieId") String movieId) {
+//        MovieSummary movieSummary = restTemplate.getForObject(movieId, MovieSummary.class);
+        return new Movie(movieId, "movieTitle", "movieOverView");
+
+    }
+}
+
